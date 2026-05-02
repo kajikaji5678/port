e0b83e628dfaa17f560c4fe49d732c97d239ee53
kajikaji5678 <bestlife.fukudakajito@gmail.com>
2026-05-02 16:05:58 +0900

    viteによりChart.jsの導入

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const target = document.querySelectorAll('.about_us_achieve');
 import Chart from 'chart.js/auto';
+const target = document.querySelectorAll('.about_us_achieve');
 
 const obs = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
@@ -15,3 +15,16 @@ target.forEach(t => obs.observe(t));
 
 console.log(document.querySelectorAll(".about_us_achieve"));
 
+new Chart(document.getElementById('about_us_chart_1'), {
+    type: 'pie',
+    data: {
+        labels: ['A', 'B', 'C'],
+        datasets: [{
+            data: [30, 50, 20]
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false
+    }
+});
