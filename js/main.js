70c514463b423e4dddbe996e76a89f06e5096346
kajikaji5678 <bestlife.fukudakajito@gmail.com>
2026-05-02 16:57:15 +0900

    セクション二つ目の大枠の粗削りのみ

--- js/main.js
+++ js/main.js
@@ -15,16 +15,16 @@ target.forEach(t => obs.observe(t));
 
 console.log(document.querySelectorAll(".about_us_achieve"));
 
-new Chart(document.getElementById('about_us_chart_1'), {
-    type: 'pie',
-    data: {
-        labels: ['A', 'B', 'C'],
-        datasets: [{
-            data: [30, 50, 20]
-        }]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false
-    }
-});
+// new Chart(document.getElementById('about_us_chart_1'), {
+//     type: 'pie',
+//     data: {
+//         labels: ['A', 'B', 'C'],
+//         datasets: [{
+//             data: [30, 50, 20]
+//         }]
+//     },
+//     options: {
+//         responsive: true,
+//         maintainAspectRatio: false
+//     }
+// });
