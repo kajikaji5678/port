3ea072454f31caccddbe4bb488aaae78cd14fb7a
kajikaji5678 <bestlife.fukudakajito@gmail.com>
2026-05-09 16:53:23 +0900

    更新

---
+++ js/data.js
@@ -0,0 +1,29 @@
+export const interviewData = [
+    {
+        name: "山田太郎",
+        job: "webライター",
+        content: "自分が執筆した記事が累計PV3万を突破しました",
+        image: "img/member1.jpg"
+    },
+
+    {
+        name: "佐藤花子",
+        job: "web制作",
+        content: "UI改善でCV率が120%向上しました",
+        image: "img/number3.jpg"
+    },
+
+    {
+        name: "鈴木一郎",
+        job: "SNS運用",
+        content: "入社3年目でチーフを務めています",
+        image: "img/member2.jpg"
+    },
+
+    {
+        name: "岡崎健斗",
+        job: "web制作",
+        content: "デザイナーの育成に努めています",
+        image: "img/number4.jpg"
+    }
+];
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // import Chart from 'chart.js/auto';
 const target = document.querySelectorAll('.about_us_achieve');
+import { interviewData } from "./data.js";
 
 document.addEventListener('DOMContentLoaded', function () {
     const tabs = document.querySelectorAll('.tab');
@@ -17,6 +18,47 @@ document.addEventListener('DOMContentLoaded', function () {
             tabs[index].classList.add('active')
         });
     });
+});
+
+// スライダー
+const cards = document.querySelectorAll(".card");
+const next = document.querySelector(".interview_arrow");
+const prev = document.querySelector(".interview_arrow_2");
+
+let start = 0;
+const dataLength = interviewData.length;
+const visibleCount = cards.length;
+
+function render() {
+
+    cards.forEach((card, index) => {
+
+        const item = interviewData[(start + index) % dataLength];
+
+        card.innerHTML = `
+            <div class="interview_image">
+                <img src="${item.image}">
+            </div>
+
+            <div class="detail ${index === 0 ? 'show' : 'hide'}">
+                <p class="detail_name">${item.name}</p>
+                <p class="detail_job">${item.job}</p>
+                <p>${item.content}</p>
+            </div>
+        `;
+    });
+}
+
+render();
+
+next.addEventListener("click", () => {
+    start = (start + 1) % dataLength;
+    render();
+});
+
+prev.addEventListener("click", () => {
+    start = (start - 1 + dataLength) % dataLength;
+    render();
 });
 
 // forEachメゾットでは第二引数でインデックスを取得できる
